90ee6a91bb9043cdb55d1dd6a058126e0eb453ae
simon-ne <57033956+simon-ne@users.noreply.github.com>
2025-03-12 22:16:38 +0100

    Fix merge conflicts, update containerization.

--- dev-docker-entrypoint.sh
+++ dev-docker-entrypoint.sh
@@ -3,11 +3,11 @@
 # Check if SEED_DB environment variable is set to true
 if [ "$SEED_DB" = "true" ]; then
   echo "Running data generator..."
-  cd /app/src/FoP_IMT.DataGenerator
+  cd /app/src/FrontEASE.DataGenerator
   dotnet run
 fi
 
 # Run the server project
 echo "Starting server..."
-cd /app/src/FoP_IMT.Server
-dotnet run --urls "http://*:5000"
+cd /app/src/FrontEASE.Server
+dotnet run --urls "http://*:4000"
