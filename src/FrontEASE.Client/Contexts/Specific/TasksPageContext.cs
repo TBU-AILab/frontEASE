0328755576402cad64b2b25c382b7fc0932b6985
j_kovac <juniorkovac@gmail.com>
2025-12-17 00:45:42 +0100

    Adjust parametrization and context - Tasks page, all modals

--- src/FrontEASE.Client/Contexts/Specific/TasksPageContext.cs
+++ src/FrontEASE.Client/Contexts/Specific/TasksPageContext.cs
@@ -1,4 +1,11 @@
-﻿namespace FrontEASE.Client.Contexts.Specific
+﻿using FrontEASE.Shared.Data.DTOs.Companies;
+using FrontEASE.Shared.Data.DTOs.Shared.Users;
+using FrontEASE.Shared.Data.DTOs.Tasks;
+using FrontEASE.Shared.Data.DTOs.Tasks.Data.Configs.Modules.Options;
+using FrontEASE.Shared.Data.DTOs.Tasks.UI;
+using System.Collections.ObjectModel;
+
+namespace FrontEASE.Client.Contexts.Specific
 {
     public class TasksPageContext : ApplicationContext
     {
@@ -6,6 +13,19 @@
         {
             LoggedInUser = appContext.LoggedInUser;
             UserPreferences = appContext.UserPreferences;
+
+            ViewMetadata = new();
+            Tasks = [];
+            TaskModuleOptions = [];
+            AvailableUsers = [];
+            AvailableCompanies = [];
         }
+
+        public TaskListViewMetadataDto ViewMetadata { get; set; }
+        public ObservableCollection<TaskInfoDto> Tasks { get; set; }
+
+        public IList<TaskModuleNoValidationDto> TaskModuleOptions { get; set; }
+        public IList<ApplicationUserDto> AvailableUsers { get; set; }
+        public IList<CompanyDto> AvailableCompanies { get; set; }
     }
 }
