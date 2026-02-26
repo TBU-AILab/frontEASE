d3468f9683ae2ab91a969aaedcafe067b74bfa19
j_kovac <juniorkovac@gmail.com>
2026-02-26 23:10:52 +0100

    Fix charts - migrate to Apex Charts

--- src/FrontEASE.Client/Infrastructure/Utils/ColorHelper.cs
+++ src/FrontEASE.Client/Infrastructure/Utils/ColorHelper.cs
@@ -1,27 +1,25 @@
-﻿using Blazorise.Charts;
-
-namespace FrontEASE.Client.Infrastructure.Utils
+﻿namespace FrontEASE.Client.Infrastructure.Utils
 {
     public static class ColorHelper
     {
-        public static string HtmlToChartRGBA(string hex, float alpha)
+        public static string HtmlToRGBA(string hex, float alpha)
         {
             if (string.IsNullOrWhiteSpace(hex))
             {
-                return ChartColor.FromRgba(0, 0, 0, alpha);
+                return $"rgba(0, 0, 0, {alpha})";
             }
 
             var trimmed = hex.TrimStart('#');
             if (trimmed.Length != 6)
             {
-                return ChartColor.FromRgba(0, 0, 0, alpha);
+                return $"rgba(0, 0, 0, {alpha})";
             }
 
             var r = (byte)Convert.ToInt32(trimmed[..2], 16);
             var g = (byte)Convert.ToInt32(trimmed.Substring(2, 2), 16);
             var b = (byte)Convert.ToInt32(trimmed.Substring(4, 2), 16);
 
-            return ChartColor.FromRgba(r, g, b, alpha);
+            return $"rgba({r}, {g}, {b}, {alpha})";
         }
     }
 }
